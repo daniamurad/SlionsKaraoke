334c9b3430e6d2cd1ca742745d9fccbf42215f25
daniamurad <daniamurad@gmail.com>
2017-12-12 14:29:04 +0800

    Speech recognizer addition

--- PerformScreen.js
+++ PerformScreen.js
@@ -1,6 +1,7 @@
-
 import React from 'react';
-import { StyleSheet,StackNavigator, Text, View , Image,TextInput , Button, TouchableOpacity} from 'react-native';
+import { ScrollView, StyleSheet,StackNavigator, Text, View , Image,TextInput , Button, TouchableOpacity, TouchableHighlight} from 'react-native';
+import SpeechTranslatorModule from 'react-native-google-speech';
+import Voice from 'react-native-voice';
 
 export class PerformScreen extends React.Component {
     static navigationOptions = {
@@ -9,6 +10,124 @@ export class PerformScreen extends React.Component {
       headerStyle:  styles.PerformScreenHeaderStyle,
       headerTitleStyle: styles.ScreenHeaderTitleStyle,
     };
+
+    constructor(props) {
+      
+      super(props);
+      this.state = {
+        recognized: '',
+        pitch: '',
+        error: '',
+        end: '',
+        started: '',
+        results: [],
+        partialResults: [],
+      };
+      Voice.onSpeechStart = this.onSpeechStart.bind(this);
+      Voice.onSpeechRecognized = this.onSpeechRecognized.bind(this);
+      Voice.onSpeechEnd = this.onSpeechEnd.bind(this);
+      Voice.onSpeechError = this.onSpeechError.bind(this);
+      Voice.onSpeechResults = this.onSpeechResults.bind(this);
+      Voice.onSpeechPartialResults = this.onSpeechPartialResults.bind(this);
+      Voice.onSpeechVolumeChanged = this.onSpeechVolumeChanged.bind(this);
+      this.flag = 0;
+    }
+
+    onSpeechStart(e) {
+      this.setState({
+        started: '√',
+      });
+    }
+    onSpeechRecognized(e) {
+      this.setState({
+        recognized: '√',
+      });
+    }
+  
+    onSpeechEnd(e) {
+      this.setState({
+        end: '√',
+      });
+    }
+  
+    onSpeechError(e) {
+      this.setState({
+        error: JSON.stringify(e.error),
+      });
+    }
+  
+    onSpeechResults(e) {
+      this.setState({
+        results: e.value,
+      });
+    }
+  
+    onSpeechPartialResults(e) {
+      this.setState({
+        partialResults: e.value,
+      });
+    }
+  
+    onSpeechVolumeChanged(e) {
+      this.setState({
+        pitch: e.value,
+      });
+    }
+  
+    async _startRecognizing(e) {
+      this.setState({
+        recognized: '',
+        pitch: '',
+        error: '',
+        started: '',
+        results: [],
+        partialResults: [],
+        end: ''
+      });
+      try {
+        await Voice.start('en-US');
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  
+    async _stopRecognizing(e) {
+      try {
+        await Voice.stop();
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  
+    async _cancelRecognizing(e) {
+      try {
+        await Voice.cancel();
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  
+    async _destroyRecognizer(e) {
+      try {
+        await Voice.destroy();
+      } catch (e) {
+        console.error(e);
+      }
+      this.setState({
+        recognized: '',
+        pitch: '',
+        error: '',
+        started: '',
+        results: [],
+        partialResults: [],
+        end: ''
+      });
+    }
+  
+
+    _testGoogle() {
+    }
+    
     render() {
       return (
 
@@ -27,9 +146,88 @@ export class PerformScreen extends React.Component {
             </View>
             <Image style={styles.StyleLearnSongIcon} source={require('./icons/frozen.jpg')}  />
         </View>
+<ScrollView>
+        <View style={{backgroundColor:'#BFC9CA', margin: 15, height: 300}}>
 
-        <View style={{backgroundColor:'#BFC9CA', margin: 15, height: 250}}>
-            <View style = {{margin: 20}}>
+        <Text
+          style={styles.stat}>
+          {`Started: ${this.state.started}`}
+        </Text>
+
+        <Text
+          style={styles.stat}>
+          {`Recognized: ${this.state.recognized}`}
+        </Text>
+        <Text
+          style={styles.stat}>
+          {`Pitch: ${this.state.pitch}`}
+        </Text>
+
+        <Text
+          style={styles.stat}>
+          {`Error: ${this.state.error}`}
+        </Text>
+        <Text
+          style={styles.stat}>
+          Results
+        </Text>
+
+        {this.state.results.map((result, index) => {
+          return (
+            <Text
+              key={`result-${index}`}
+              style={styles.stat}>
+              {result}
+            </Text>
+          )
+        })}
+
+           <Text
+          style={styles.stat}>
+          Partial Results
+        </Text>
+        {this.state.partialResults.map((result, index) => {
+          return (
+            <Text
+              key={`partial-result-${index}`}
+              style={styles.stat}>
+              {result}
+            </Text>
+          )
+        })}
+
+        <Text
+          style={styles.stat}>
+          {`End: ${this.state.end}`}
+        </Text>
+
+        <TouchableOpacity onPress={this._startRecognizing.bind(this)}>
+          <Image
+            style={styles.button}
+            source={require('./button.png')}
+          />
+          </TouchableOpacity>
+
+          <TouchableHighlight onPress={this._stopRecognizing.bind(this)}>
+          <Text
+            style={styles.action}>
+            Stop Recognizing
+          </Text>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={this._cancelRecognizing.bind(this)}>
+          <Text
+            style={styles.action}>
+            Cancel
+          </Text>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={this._destroyRecognizer.bind(this)}>
+          <Text
+            style={styles.action}>
+            Destroy
+          </Text>
+        </TouchableHighlight>
+
+            {/* <View style = {{margin: 20}}>
                 <Text style={styles.TextStyleSongCurrentLyrics}>Snow glows white on the mountain tonight</Text>
                 <Text style={styles.TextStyleSongCurrentLyrics}>雪今晚在山上发出白光</Text>
             </View>
@@ -37,14 +235,22 @@ export class PerformScreen extends React.Component {
             <View style = {{margin: 20}}>
                 <Text style={styles.TextStyleSongNextLyrics}>Not a footprint to be seen</Text>
                 <Text style={styles.TextStyleSongNextLyrics}>不是被看见的脚印</Text>
-            </View>
+            </View> */}
         </View>
+        </ScrollView>
 
-
-        <TouchableOpacity onPress={() => this.props.navigation.navigate('Feedback')} style = {{alignSelf:'center'}}>
+        <View style={{ 
+            flex: 1,
+            flexDirection: 'row'
+        }}>
+        {/* <TouchableOpacity onPress={() => this.props.navigation.navigate('Feedback')} style = {{alignSelf:'auto'}}>
              <Image style={styles.PlayBackIcons} source={require('./icons/red_mic.gif')}  />
              </TouchableOpacity>
-             
+
+        <TouchableOpacity onPress={this._testGoogle.bind(this)} style = {{alignSelf:'auto'}}>
+             <Image style={styles.PlayBackIcons} source={require('./icons/red_mic.gif')}  />
+        </TouchableOpacity> */}
+             </View>
             </View>
       );
     }
--- styles.js
+++ styles.js
@@ -256,4 +256,29 @@ GenreImageStyle: {
             backgroundColor: '#BFC9CA',
             color:'red'
         },
+        welcome: {
+          fontSize: 20,
+          textAlign: 'center',
+          margin: 10,
+        },
+        action: {
+          textAlign: 'center',
+          color: '#0000FF',
+          marginVertical: 5,
+          fontWeight: 'bold',
+        },
+        instructions: {
+          textAlign: 'center',
+          color: '#333333',
+          marginBottom: 5,
+        },
+        stat: {
+          textAlign: 'center',
+          color: '#B0171F',
+          marginBottom: 1,
+        },
+        button: {
+          width: 30,
+          height: 30,
+        },
 });
